1316af21cf43f9c7e385cc460d91c90d49577a2f
Anastasia <anastasijatsar@mail.ru>
2022-02-22 18:49:17 +0300

    Изменения в вспомогательных функциях

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function getRandomInt(min, max) {
   if (min < 0 || max <= min) {
-    return('Некорректное значение');
+    return 0;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInt();
+getRandomInt(-5, 10);
 
 function getRandomFloat(min, max, digits) {
   if (min < 0 || max <= min) {
-    return('Некорректное значение');
+    return 0;
   }
   return ((Math.random() * (max - min + 1)) + min).toFixed(digits);
 }
 
-getRandomFloat();
+getRandomFloat(5, 10, 2);
