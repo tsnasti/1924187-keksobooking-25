8db5255d46856f1ca2e3f20bde836cd4fbe5471e
Anastasia <anastasijatsar@mail.ru>
2022-03-16 16:14:23 +0300

    Изменения в работе с событиями (часть)

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ createSimilarAdvertisement();
 
 import {appendSimilarAdvertisement} from './card.js';
 
-appendSimilarAdvertisement(createSimilarAdvertisement());
-
 import {addDisabledStatus} from './form-status.js';
-
-addDisabledStatus();
 
 import {addActiveStatus} from './form-status.js';
 
-addActiveStatus();
-
 import './user-form.js';
+
+appendSimilarAdvertisement(createSimilarAdvertisement());
+
+addDisabledStatus();
+
+addActiveStatus();
--- js/user-form.js
+++ js/user-form.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('.ad-form');
 const roomNumber = form.querySelector('[name="rooms"]');
 const capacityNumber = form.querySelector('[name="capacity"]');
+const houseroomTypes = form.querySelector('[name="type"]');
+const price = form.querySelector('[name="price"]');
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
@@ -8,17 +10,44 @@ const pristine = new Pristine(form, {
 });
 
 const roomForCapacity = {
-  '1':['1'],
-  '2':['1', '2'],
-  '3':['1', '2', '3'],
-  '100':['0']
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+const houseroomMinPrice = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
 };
 
 function capacityValidation () {
   return roomForCapacity[roomNumber.value].includes(capacityNumber.value);
 }
 
-pristine.addValidator(capacityNumber, capacityValidation, 'Неправильное значение');
+pristine.addValidator(capacityNumber, capacityValidation, 'Колличество гостей не может превышать количество комнат');
+
+function setMinPriceAttr () {
+  price.min = houseroomMinPrice[houseroomTypes.value];
+  price.placeholder = houseroomMinPrice[houseroomTypes.value];
+}
+
+houseroomTypes.addEventListener('change', setMinPriceAttr);
+
+const ErrorMassagePrice = () => `Минимальная цена ${price.min}`;
+
+function minPriceValidation () {
+  if (price.value >= price.min) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+pristine.addValidator(price, minPriceValidation, ErrorMassagePrice);
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
